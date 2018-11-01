3791d5f3edd314e464c191c3a88c45b6e5df0b93
whayes <whayes@gaig.com>
2018-11-01 14:47:33 -0400

    latest update
    this is the descripti

---
+++ Test Suites/TS_SmokeTest.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_SmokeTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>24c6c2d7-d490-4075-a88a-657fc1a99299</testSuiteGuid>
+   <testCaseLink>
+      <guid>c1b04962-1245-4576-82ef-a6481dbcb856</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_GoogleSearch - ForceFailure</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99eea77b-d33d-45ff-807c-09890b7f281e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_MakeAppt</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>273c0544-fcff-4d4d-b304-aa95e36e9f0f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_GoogleSearch</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
